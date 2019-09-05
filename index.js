5cdcfea486226c39ffd10fd0f625ba9bb43fa850
Brandon Pittman <brandon@brandonpittman.net>
2019-09-05 17:28:31 +0900

    add custom forms

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-module.exports = function ({addUtilities, addVariant, variants, e}) {
+const postcss = require('postcss')
+
+module.exports = function ({addComponents, addUtilities, addVariant, theme, variants, e}) {
   require("tailwindcss-plugin-transitions")({addUtilities, variants});
   require("tailwindcss-plugin-content")({addUtilities, addVariant, e});
   require("tailwindcss-plugin-aspect")({addUtilities, variants});
   require("tailwindcss-plugin-current-color")({addUtilities});
   require("tailwindcss-plugin-prefers-color-scheme")({addVariant, e});
+  require("@tailwindcss/custom-forms")({addUtilities, addComponents, theme, postcss})
 }
+
