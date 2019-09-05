5c75bd1be0d39c8e5ecc13306b2b10459e49f789
Brandon Pittman <brandon@brandonpittman.net>
2019-09-05 17:49:42 +0900

    wip

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const postcss = require('postcss')
 
 module.exports = function ({addComponents, addUtilities, addVariant, theme, variants, e}) {
   require("tailwindcss-plugin-transitions")({addUtilities, variants});
-  require("tailwindcss-plugin-content")({addUtilities, addVariant, e});
+  require("tailwindcss-plugin-content")({addComponents, addUtilities, addVariant, e});
   require("tailwindcss-plugin-aspect")({addUtilities, variants});
   require("tailwindcss-plugin-current-color")({addUtilities});
   require("tailwindcss-plugin-prefers-color-scheme")({addVariant, e});
