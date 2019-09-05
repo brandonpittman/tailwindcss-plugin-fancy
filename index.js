c92a0d62376b49eba89bccf128d2c29b6868bf1b
Brandon Pittman <brandon@brandonpittman.net>
2019-09-05 17:00:16 +0900

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function ({addUtilities, addVariant, variants, e}) {
+  require("tailwindcss-plugin-transitions")({addUtilities, variants});
   require("tailwindcss-plugin-content")({addUtilities, addVariant, e});
   require("tailwindcss-plugin-aspect")({addUtilities, variants});
   require("tailwindcss-plugin-current-color")({addUtilities});
   require("tailwindcss-plugin-prefers-color-scheme")({addVariant, e});
-
 }
