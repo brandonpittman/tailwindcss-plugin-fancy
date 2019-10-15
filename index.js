abb3bf2510b1d75a993abc115fb14e56a7564edc
Brandon Pittman <brandon@brandonpittman.net>
2019-10-15 18:23:33 +0900

    wip

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ module.exports = function ({addComponents, addUtilities, addVariant, theme, vari
   require("tailwindcss-plugin-content")({addComponents, addUtilities, addVariant, e});
   require("tailwindcss-plugin-aspect")({addUtilities, variants});
   require("tailwindcss-plugin-decoration")({addUtilities, variants, theme});
-  //require("tailwindcss-plugin-prefers-color-scheme")({addVariant, e});
   require("@tailwindcss/custom-forms")({addUtilities, addComponents, theme, postcss})
 }
 
