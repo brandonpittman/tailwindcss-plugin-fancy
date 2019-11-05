ea8c9371e5b37f5342394556d0be09955de87851
Brandon Pittman <brandon@brandonpittman.net>
2019-11-05 13:33:09 +0900

    wip

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-module.exports = bagOfCrap => {
-  require("tailwindcss-plugin-animated")(bagOfCrap)
-  require("tailwindcss-plugin-transitions")(bagOfCrap)
-  require("tailwindcss-plugin-content")(bagOfCrap)
-  require("tailwindcss-plugin-aspect")(bagOfCrap)
-  require("tailwindcss-plugin-decoration")(bagOfCrap)
-  require("@tailwindcss/custom-forms")(bagOfCrap)
-}
+// Module.exports = bagOfCrap => {
+//   require("tailwindcss-plugin-animated")(bagOfCrap)
+//   require("tailwindcss-plugin-transitions")(bagOfCrap)
+//   require("tailwindcss-plugin-content")(bagOfCrap)
+//   require("tailwindcss-plugin-aspect")(bagOfCrap)
+//   require("tailwindcss-plugin-decoration")(bagOfCrap)
+//   require("@tailwindcss/custom-forms")(bagOfCrap)
+// }
 
 // This is what the plugin will look like when 1.2 goes live
 
-// module.exports = {
-//   ...require("tailwindcss-plugin-prefers-color-scheme"),
-//   handler(bagOfCrap) {
-//     require("tailwindcss-plugin-animated")(bagOfCrap)
-//     require("tailwindcss-plugin-transitions")(bagOfCrap)
-//     require("tailwindcss-plugin-content")(bagOfCrap)
-//     require("tailwindcss-plugin-aspect")(bagOfCrap)
-//     require("tailwindcss-plugin-decoration")(bagOfCrap)
-//     require("@tailwindcss/custom-forms")(bagOfCrap)
-//   }
-// }
+module.exports = {
+	...require('tailwindcss-plugin-prefers-color-scheme'),
+	handler(bagOfCrap) {
+		require('tailwindcss-plugin-animated')(bagOfCrap);
+		require('tailwindcss-plugin-transitions')(bagOfCrap);
+		require('tailwindcss-plugin-content')(bagOfCrap);
+		require('tailwindcss-plugin-aspect')(bagOfCrap);
+		require('tailwindcss-plugin-decoration')(bagOfCrap);
+		require('@tailwindcss/custom-forms')(bagOfCrap);
+	}
+};
