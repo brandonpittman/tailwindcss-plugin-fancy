9923f4c2e8002506cc7ee5cffe1f7586a0578e15
Brandon Pittman <brandon@brandonpittman.net>
2020-01-09 18:37:31 +0900

    Add design plugin

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const plugin = require('tailwindcss/plugin')
 const content = require('tailwindcss-plugin-content')
+const design = require('tailwindcss-plugin-design')
 
 module.exports = plugin(function (bagOfCrap) {
   content.handler(bagOfCrap)
@@ -9,37 +10,5 @@ module.exports = plugin(function (bagOfCrap) {
   require('@tailwindcss/custom-forms')(bagOfCrap);
 }, {
   ...content.config,
-  theme: {
-    extend: {
-      inset: {
-        "1/4": "25%",
-        "1/3": "33.333333%",
-        "2/3": "66.666666%",
-        "1/2": "50%",
-        "3/4": "75%",
-        "full": "100%",
-      },
-      skew: {
-        '0': '0deg',
-        '1': '1deg',
-        '2': '2deg',
-        '3': '3deg',
-        '4': '4deg',
-        '5': '5deg',
-        '10': '10deg',
-        '15': '15deg',
-        '20': '20deg',
-        '90': '90deg',
-        '-1': '-1deg',
-        '-2': '-2deg',
-        '-3': '-3deg',
-        '-4': '-4deg',
-        '-5': '-5deg',
-        '-10': '-10deg',
-        '-15': '-15deg',
-        '-20': '-20deg',
-        '-90': '-90deg',
-      }
-    },
-  }
+  ...design.config
 })
