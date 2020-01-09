34156c6b799620a33096f559e6b13970bd6efeeb
Brandon Pittman <brandon@brandonpittman.net>
2020-01-09 12:35:27 +0900

    Update deps

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
+const plugin = require('tailwindcss/plugin')
 const content = require('tailwindcss-plugin-content')
 
-module.exports = {
-  config: {
-    ...require('tailwindcss-plugin-prefers-color-scheme').config,
-    ...content.config
-  },
-  handler(bagOfCrap) {
-    content.handler(bagOfCrap);
-    require('tailwindcss-plugin-animated')(bagOfCrap);
-    require('tailwindcss-plugin-transitions')(bagOfCrap);
-    require('tailwindcss-plugin-aspect')(bagOfCrap);
-    require('tailwindcss-plugin-decoration')(bagOfCrap);
-    require('@tailwindcss/custom-forms')(bagOfCrap);
+module.exports = plugin(function (bagOfCrap) {
+  content.handler(bagOfCrap)
+  require('tailwindcss-plugin-animated').handler(bagOfCrap);
+  require('tailwindcss-plugin-aspect').handler(bagOfCrap);
+  require('tailwindcss-plugin-decoration').handler(bagOfCrap);
+  require('@tailwindcss/custom-forms')(bagOfCrap);
+}, {
+  ...content.config,
+  theme: {
+    extend: {
+      inset: {
+        "1/4": "25%",
+        "1/3": "33.333333%",
+        "2/3": "66.666666%",
+        "1/2": "50%",
+        "3/4": "75%",
+        "full": "100%",
+      },
+      skew: {
+        '0': '0deg',
+        '1': '1deg',
+        '2': '2deg',
+        '3': '3deg',
+        '4': '4deg',
+        '5': '5deg',
+        '10': '10deg',
+        '15': '15deg',
+        '20': '20deg',
+        '90': '90deg',
+        '-1': '-1deg',
+        '-2': '-2deg',
+        '-3': '-3deg',
+        '-4': '-4deg',
+        '-5': '-5deg',
+        '-10': '-10deg',
+        '-15': '-15deg',
+        '-20': '-20deg',
+        '-90': '-90deg',
+      }
+    },
   }
-};
+})
