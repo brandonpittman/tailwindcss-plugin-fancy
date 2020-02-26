aa9ff4ee390715c3e11d2d2e9139048c1475eb70
Brandon Pittman <brandon@brandonpittman.net>
2020-02-26 12:37:54 +0900

    Create monorepo

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const plugin = require('tailwindcss/plugin')
+const animated = require('./packages/animated')
+const aspect = require('./packages/aspect')
 const content = require('./packages/content')
+const decoration = require('./packages/decoration')
 const design = require('./packages/design')
 const future = require('./packages/future')
-const animation = require('./packages/animated')
-const aspect = require('./packages/aspect')
-const decoration = require('./packages/decoration')
 const forms = require('@tailwindcss/custom-forms')
 
 module.exports = plugin(function (bagOfCrap) {
   content.handler(bagOfCrap)
   design.handler(bagOfCrap)
   future.handler(bagOfCrap)
-  animation.handler(bagOfCrap);
+  animated.handler(bagOfCrap);
   aspect.handler(bagOfCrap);
   decoration.handler(bagOfCrap);
   forms(bagOfCrap);
@@ -28,3 +28,10 @@ module.exports = plugin(function (bagOfCrap) {
     },
   },
 })
+
+module.exports.animated = animated
+module.exports.aspect = aspect
+module.exports.content = content
+module.exports.decoration = decoration
+module.exports.design = design
+module.exports.future = future
