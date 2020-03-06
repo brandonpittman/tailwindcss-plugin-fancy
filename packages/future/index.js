f6eea18d70d02afb5ca35655e735142c6f473d79
Brandon Pittman <brandon@brandonpittman.net>
2020-03-06 23:49:24 +0900

    🎨 Add TailwindUI support

--- packages/future/index.js
+++ packages/future/index.js
@@ -1,53 +1,19 @@
 const plugin = require('tailwindcss/plugin');
 
-module.exports = plugin(({addUtilities, theme}) => {
+module.exports = plugin(({addUtilities, e, theme}) => {
 	const spacing = Object.entries(theme('spacing'));
 
 	for (const [prop, value] of spacing) {
 		addUtilities({
-			[`.space-y-${prop} > * + *`]: {
+			[`.space-y-${e(prop)} > * + *`]: {
 				marginTop: value
 			}
 		}, ['responsive']);
 
 		addUtilities({
-			[`.space-x-${prop} > * + *`]: {
+			[`.space-x-${e(prop)} > * + *`]: {
 				marginLeft: value
 			}
 		}, ['responsive']);
 	}
-}, {
-	theme: {
-		extend: {
-			inset: {
-				'1/4': '25%',
-				'1/3': '33.333333%',
-				'2/3': '66.666666%',
-				'1/2': '50%',
-				'3/4': '75%',
-				full: '100%'
-			},
-			skew: {
-				0: '0deg',
-				1: '1deg',
-				2: '2deg',
-				3: '3deg',
-				4: '4deg',
-				5: '5deg',
-				10: '10deg',
-				15: '15deg',
-				20: '20deg',
-				90: '90deg',
-				'-1': '-1deg',
-				'-2': '-2deg',
-				'-3': '-3deg',
-				'-4': '-4deg',
-				'-5': '-5deg',
-				'-10': '-10deg',
-				'-15': '-15deg',
-				'-20': '-20deg',
-				'-90': '-90deg'
-			}
-		}
-	}
-});
+}, {});
