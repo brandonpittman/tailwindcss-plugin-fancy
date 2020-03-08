7ad7c20c1afb8a9a10c4809a93f66486c834c22d
Brandon Pittman <brandon@brandonpittman.net>
2020-03-08 10:10:48 +0900

    wip

--- index.js
+++ index.js
@@ -9,45 +9,45 @@ const heroicons = require('./packages/heroicons');
 const ui = require('@tailwindcss/ui')();
 
 module.exports = plugin.withOptions(() => {
-	return function (bagOfCrap) {
-		content.handler(bagOfCrap);
-		design.handler(bagOfCrap);
-		future.handler(bagOfCrap);
-		animated.handler(bagOfCrap);
-		aspect.handler(bagOfCrap);
-		decoration.handler(bagOfCrap);
-		ui.handler(bagOfCrap);
-		heroicons({
-			variants: [
-				'responsive',
-				'hover',
-				'focus',
-				'active',
-				'group-hover'
-			]
-		}).handler(bagOfCrap);
-	};
+  return function (bagOfCrap) {
+    content.handler(bagOfCrap);
+    design.handler(bagOfCrap);
+    future.handler(bagOfCrap);
+    animated.handler(bagOfCrap);
+    aspect.handler(bagOfCrap);
+    decoration.handler(bagOfCrap);
+    ui.handler(bagOfCrap);
+    heroicons({
+      variants: [
+        'responsive',
+        'hover',
+        'focus',
+        'active',
+        'group-hover'
+      ]
+    }).handler(bagOfCrap);
+  };
 }, _ => {
-	return {
-		variants: {
-			...content.config.variants
-			// ...ui.config.variants
-		},
-		theme: {
-			spacing: {...ui.config.theme.spacing},
-			colors: {...ui.config.theme.colors},
-			inset: (theme, options) => ({...ui.config.theme.inset(theme, options)}),
-			maxWidth: (theme, options) => ({...ui.config.theme.maxWidth(theme, options)}),
-			maxHeight: (theme, options) => ({...ui.config.theme.maxHeight(theme, options)}),
-			customForms: (theme, options) => ({...ui.config.theme.customForms(theme, options)}),
-			boxShadow: {...ui.config.theme.boxShadow},
-			extend: {
-				screens: {
-					...design.config.theme.extend.screens
-				}
-			}
-		}
-	};
+  return {
+    variants: {
+      ...content.config.variants,
+      ...ui.config.variants
+    },
+    theme: {
+      spacing: {...ui.config.theme.spacing},
+      colors: {...ui.config.theme.colors},
+      inset: (theme, options) => ({...ui.config.theme.inset(theme, options)}),
+      maxWidth: (theme, options) => ({...ui.config.theme.maxWidth(theme, options)}),
+      maxHeight: (theme, options) => ({...ui.config.theme.maxHeight(theme, options)}),
+      customForms: (theme, options) => ({...ui.config.theme.customForms(theme, options)}),
+      boxShadow: {...ui.config.theme.boxShadow},
+      extend: {
+        screens: {
+          ...design.config.theme.extend.screens
+        }
+      }
+    }
+  };
 });
 
 module.exports.animated = animated;
