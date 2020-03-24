f9706a82fb24a5fcd0d1bdefc772eeb0f6dc6584
Brandon Pittman <github@brandonpittman.net>
2020-03-24 10:58:47 +0900

    ✨ Add transition-delay utilities to match durations

--- packages/future/index.js
+++ packages/future/index.js
@@ -1,11 +1,26 @@
 const plugin = require('tailwindcss/plugin');
 
-module.exports = plugin(({addComponents, e, theme}) => {
-	addComponents({
+module.exports = plugin(({addUtilities, addComponents, e, theme}) => {
+	addUtilities({
 		'.keep-all': {
 			'word-break': 'break-all'
 		}
 	});
+
+	const durations = Object.entries(theme('transitionDuration'));
+	const delays = {
+		'.delay-none': {
+			transitionDelay: '0s'
+		}
+	};
+
+	for (const [prop, value] of durations) {
+		delays[`.delay-${prop}`] = {
+			transitionDelay: value
+		};
+	}
+
+	addUtilities(delays, ['responsive']);
 
 	const spacing = Object.entries(theme('spacing'));
 
