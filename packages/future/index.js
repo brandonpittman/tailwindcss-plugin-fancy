164413835d84df0acd75115e7589ea952ac6a645
Brandon Pittman <github@brandonpittman.net>
2020-03-24 16:04:12 +0900

    Add responsive variants of space-{x,y} components

--- packages/future/index.js
+++ packages/future/index.js
@@ -1,40 +1,44 @@
 const plugin = require('tailwindcss/plugin');
 
 module.exports = plugin(({addUtilities, addComponents, e, theme}) => {
-	addUtilities({
-		'.keep-all': {
-			'word-break': 'break-all'
-		}
-	});
+  addUtilities({
+    '.keep-all': {
+      'word-break': 'break-all'
+    }
+  });
 
-	const durations = Object.entries(theme('transitionDuration'));
-	const delays = {
-		'.delay-none': {
-			transitionDelay: '0s'
-		}
-	};
+  const durations = Object.entries(theme('transitionDuration'));
+  const delays = {
+    '.delay-none': {
+      transitionDelay: '0s'
+    }
+  };
 
-	for (const [prop, value] of durations) {
-		delays[`.delay-${prop}`] = {
-			transitionDelay: value
-		};
-	}
+  for (const [prop, value] of durations) {
+    delays[`.delay-${prop}`] = {
+      transitionDelay: value
+    };
+  }
 
-	addUtilities(delays, ['responsive']);
+  addUtilities(delays, ['responsive']);
 
-	const spacing = Object.entries(theme('spacing'));
+  const spacing = Object.entries(theme('spacing'));
 
-	for (const [prop, value] of spacing) {
-		addComponents({
-			[`.space-y-${e(prop)} > * + *`]: {
-				marginTop: value
-			}
-		}, ['responsive']);
+  for (const [prop, value] of spacing) {
+    addComponents({
+      '@variants responsive': {
+        [`.space-y-${e(prop)} > * + *`]: {
+          marginTop: value
+        }
+      }
+    }, ['responsive']);
 
-		addComponents({
-			[`.space-x-${e(prop)} > * + *`]: {
-				marginLeft: value
-			}
-		}, ['responsive']);
-	}
+    addComponents({
+      '@variants responsive': {
+        [`.space-x-${e(prop)} > * + *`]: {
+          marginLeft: value
+        }
+      }
+    }, ['responsive']);
+  }
 }, {});
