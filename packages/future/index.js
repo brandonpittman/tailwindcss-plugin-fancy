bcaeb9516f1577453f2b7a4c360d6303e29cd7eb
Brandon Pittman <github@brandonpittman.net>
2020-04-21 11:40:43 +0900

    ⬆️ Upgrade to Tailwind 1.3
    Removes /future space features because they are in core now.

--- packages/future/index.js
+++ packages/future/index.js
@@ -1,44 +1,9 @@
 const plugin = require('tailwindcss/plugin');
 
-module.exports = plugin(({addUtilities, addComponents, e, theme}) => {
-	addUtilities({
-		'.keep-all': {
-			'word-break': 'break-all'
-		}
-	});
-
-	const durations = Object.entries(theme('transitionDuration'));
-	const delays = {
-		'.delay-none': {
-			transitionDelay: '0s'
-		}
-	};
-
-	for (const [prop, value] of durations) {
-		delays[`.delay-${prop}`] = {
-			transitionDelay: value
-		};
-	}
-
-	addUtilities(delays, ['responsive']);
-
-	const spacing = Object.entries(theme('spacing'));
-
-	for (const [prop, value] of spacing) {
-		addComponents({
-			'@variants responsive': {
-				[`.space-y-${e(prop)} > * + *`]: {
-					marginTop: value
-				}
-			}
-		}, ['responsive']);
-
-		addComponents({
-			'@variants responsive': {
-				[`.space-x-${e(prop)} > * + *`]: {
-					marginLeft: value
-				}
-			}
-		}, ['responsive']);
-	}
+module.exports = plugin(({addUtilities}) => {
+  addUtilities({
+    '.keep-all': {
+      'word-break': 'break-all'
+    }
+  });
 }, {});
