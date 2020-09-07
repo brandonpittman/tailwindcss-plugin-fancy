b2c2445713e4249feb0da0cda60956c292df096a
Brandon Pittman <blp@hey.com>
2020-09-07 18:07:47 +0900

    wip

--- packages/future/index.js
+++ packages/future/index.js
@@ -13,13 +13,25 @@ const delays = Object.entries(defaultTheme.transitionDelay).reduce(
   {}
 );
 
-module.exports = plugin(({ addUtilities }) => {
+const widthReducer = (e) => (acc, cur) => ({
+  ...acc,
+  [`.${e(`basis-${cur[0]}`)}`]: {
+    flexBasis: cur[1],
+  },
+});
+
+
+module.exports = plugin(({ addUtilities, e }) => {
   addUtilities(
     {
       ".debug": {
         border: "1px solid red",
       },
       ...delays,
+      ...Object.entries(defaultTheme.width).reduce(
+        (acc, cur) => widthReducer(e)(acc, cur),
+        {}
+      ),
     },
     ["responsive"]
   );
