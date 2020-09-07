a83685fc7677b6da96084a6ad003f9e1c5f9fe9c
Brandon Pittman <blp@hey.com>
2020-09-07 18:11:52 +0900

    ✨ Add flex-basis utilities

--- packages/future/index.js
+++ packages/future/index.js
@@ -1,37 +1,34 @@
 const plugin = require("tailwindcss/plugin");
-const defaultTheme = require("tailwindcss/defaultTheme");
 
-const delayReducer = (acc, cur) => ({
-  ...acc,
-  [`.animate-delay-${cur[0]}`]: {
-    animationDelay: cur[1],
-  },
-});
+module.exports = plugin(({ addUtilities, theme, e }) => {
+  const delayReducer = (acc, cur) => ({
+    ...acc,
+    [`.animate-delay-${cur[0]}`]: {
+      animationDelay: cur[1],
+    },
+  });
 
-const delays = Object.entries(defaultTheme.transitionDelay).reduce(
-  delayReducer,
-  {}
-);
+  const widthReducer = (acc, cur) => ({
+    ...acc,
+    [`.${e(`basis-${cur[0]}`)}`]: {
+      flexBasis: cur[1],
+    },
+  });
 
-const widthReducer = (e) => (acc, cur) => ({
-  ...acc,
-  [`.${e(`basis-${cur[0]}`)}`]: {
-    flexBasis: cur[1],
-  },
-});
+  const delays = Object.entries(theme("transitionDelay")).reduce(
+    delayReducer,
+    {}
+  );
 
+  const basises = Object.entries(theme("width")).reduce(widthReducer, {});
 
-module.exports = plugin(({ addUtilities, e }) => {
   addUtilities(
     {
       ".debug": {
         border: "1px solid red",
       },
       ...delays,
-      ...Object.entries(defaultTheme.width).reduce(
-        (acc, cur) => widthReducer(e)(acc, cur),
-        {}
-      ),
+      ...basises,
     },
     ["responsive"]
   );
