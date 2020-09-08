e6c38b02d36fc195c940b438d2fb64aef3d1265a
Brandon Pittman <blp@hey.com>
2020-09-08 18:12:15 +0900

    WIP

--- packages/future/index.js
+++ packages/future/index.js
@@ -1,28 +1,44 @@
 const plugin = require("tailwindcss/plugin");
 
-module.exports = plugin(({ addUtilities, theme, e }) => {
-  const delays = Object.fromEntries(
-    Object.entries(theme("transitionDelay")).map(([k, v]) => [
-      `.animate-delay-${k}`,
-      { animationDelay: v },
-    ])
-  );
+module.exports = plugin(
+  ({ addUtilities, theme, e }) => {
+    const delays = Object.fromEntries(
+      Object.entries(theme("transitionDelay")).map(([k, v]) => [
+        `.animate-delay-${k}`,
+        { animationDelay: v, "--animate-delay": v },
+      ])
+    );
 
-  const basises = Object.fromEntries(
-    Object.entries(theme("width")).map(([k, v]) => [
-      `.${e(`basis-${k}`)}`,
-      { flexBasis: v },
-    ])
-  );
+    const basises = Object.fromEntries(
+      Object.entries(theme("width")).map(([k, v]) => [
+        `.${e(`basis-${k}`)}`,
+        { flexBasis: v },
+      ])
+    );
 
-  addUtilities(
-    {
-      ".debug": {
-        border: `1px solid ${theme("colors.red['600']")}`,
+    addUtilities(
+      {
+        ".debug": {
+          border: `1px solid ${theme("colors.red['600']")}`,
+        },
+        ...delays,
+        ...basises,
       },
-      ...delays,
-      ...basises,
+      ["responsive"]
+    );
+  },
+  {
+    theme: {
+      extend: {
+        animation: {
+          spin: "spin 1s linear infinite var(--animate-delay, 0s)",
+          ping:
+            "ping 1s cubic-bezier(0, 0, 0.2, 1) infinite var(--animate-delay, 0s)",
+          pulse:
+            "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite var(--animate-delay, 0s)",
+          bounce: "bounce 1s infinite var(--animate-delay, 0s)",
+        },
+      },
     },
-    ["responsive"]
-  );
-}, {});
+  }
+);
