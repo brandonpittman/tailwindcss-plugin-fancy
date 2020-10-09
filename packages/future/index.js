0ddbfcf12728a00d6f453e98982717f5ebf57897
Brandon Pittman <blp@hey.com>
2020-10-09 14:41:01 +0900

    Add .touch variant

--- packages/future/index.js
+++ packages/future/index.js
@@ -1,6 +1,7 @@
 const plugin = require("tailwindcss/plugin");
+const postcss = require("postcss");
 
-module.exports = plugin(({ addUtilities, theme, e }) => {
+module.exports = plugin(({ addVariant, addUtilities, theme, e }) => {
   const basises = Object.fromEntries(
     Object.entries(theme("width")).map(([k, v]) => [
       `.${e(`basis-${k}`)}`,
@@ -17,4 +18,16 @@ module.exports = plugin(({ addUtilities, theme, e }) => {
     },
     ["responsive"]
   );
+
+  addVariant("touch", ({ container, separator }) => {
+    const supportsRule = postcss.atRule({
+      name: "media",
+      params: "(hover: none)",
+    });
+    supportsRule.append(container.nodes);
+    container.append(supportsRule);
+    supportsRule.walkRules((rule) => {
+      rule.selector = `.${e(`touch${separator}${rule.selector.slice(1)}`)}`;
+    });
+  });
 });
